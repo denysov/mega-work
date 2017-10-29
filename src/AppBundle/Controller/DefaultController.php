cfdaaf1f5cbf4484320041a7cf780efe4e8af855
denysov <dmytro@denysov.net>
2017-10-29 11:56:55 +0200

    Test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        
         return $this->render('AppBundle:Default:index.html.twig', array(
             // ...
         ));
